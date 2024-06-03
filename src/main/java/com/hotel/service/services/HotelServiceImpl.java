17d1c58737cec64035fee074ba8b344c7a7b6928
Ajay-Babare <ajaybabare97@gmail.com>
2024-06-03 20:05:59 +0530

    hotel service done

---
+++ src/main/java/com/hotel/service/services/HotelServiceImpl.java
@@ -0,0 +1,36 @@
+package com.hotel.service.services;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hotel.service.entities.Hotel;
+import com.hotel.service.exception.ResourceNotFoundException;
+import com.hotel.service.repositories.HotelRepository;
+
+@Service
+public class HotelServiceImpl implements HotelService {
+
+	@Autowired
+	private HotelRepository hotelRepository;
+	
+	@Override
+	public Hotel createHotel(Hotel hotel) {
+		String hotelId = UUID.randomUUID().toString();
+		hotel.setId(hotelId);
+		return hotelRepository.save(hotel);
+	}
+
+	@Override
+	public List<Hotel> getAllHotels() {
+		return hotelRepository.findAll();
+	}
+
+	@Override
+	public Hotel getHotel(String id) {
+		return hotelRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Resource Not Found at Given id!!"));
+	}
+
+}
